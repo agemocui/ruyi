c308770adaebb146d69b3b448297c8f607120239
Agemo Cui <agemocui@live.com>
2017-06-10 18:31:52 -0700

    Add test for spsc

---
+++ tests/channel.rs
@@ -0,0 +1,23 @@
+extern crate ruyi;
+extern crate futures;
+
+use std::thread;
+use std::time::Duration;
+
+use futures::Stream;
+
+use ruyi::channel::spsc;
+use ruyi::reactor::{self, IntoStream};
+
+#[test]
+fn spsc() {
+    let (tx, rx) = spsc::sync_channel::<usize>(1).unwrap();
+    let handle = thread::spawn(move || {
+                                   let task = rx.into_stream().for_each(|_| Ok(()));
+                                   reactor::run(task).unwrap();
+                               });
+    tx.send(1).unwrap();
+    thread::sleep(Duration::from_millis(100));
+    drop(tx);
+    handle.join().unwrap();
+}
