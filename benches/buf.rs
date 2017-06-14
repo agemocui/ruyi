076f5604133f5b260f8afcdd3e4222a8c271eb67
Agemo Cui <agemocui@live.com>
2017-06-14 00:18:57 -0700

    Fill test ByteBuf with 0s instead of raw mem values

--- benches/buf.rs
+++ benches/buf.rs
@@ -3,8 +3,6 @@
 extern crate test;
 extern crate ruyi;
 
-use std::vec::Vec;
-
 use ruyi::buf::ByteBuf;
 use ruyi::buf::codec::*;
 
@@ -14,24 +12,16 @@ const SIZE: usize = 16 * 1024;
 
 #[bench]
 fn bench_find(b: &mut Bencher) {
-    let mut data: Vec<u8> = Vec::with_capacity(SIZE);
-    unsafe {
-        data.set_len(SIZE);
-    }
     let mut buf = ByteBuf::with_capacity(SIZE);
-    buf.append(&data[..], u8s::append).unwrap();
+    buf.append(u8s::filling(0, SIZE), u8s::append_fill).unwrap();
     let needle = b"\r\n";
     b.iter(|| { buf.find(needle); });
 }
 
 #[bench]
 fn bench_windows(b: &mut Bencher) {
-    let mut data: Vec<u8> = Vec::with_capacity(SIZE);
-    unsafe {
-        data.set_len(SIZE);
-    }
     let mut buf = ByteBuf::with_capacity(SIZE);
-    buf.append(&data[..], u8s::append).unwrap();
+    buf.append(u8s::filling(0, SIZE), u8s::append_fill).unwrap();
     let needle = b"\r\n";
     b.iter(|| { buf.windows(needle.len()).position(|w| w == needle); });
 }
