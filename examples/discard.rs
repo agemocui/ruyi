465e17d15ad24128582a232c6fb7f79ecd5e8240
Agemo Cui <agemocui@live.com>
2017-07-12 23:24:15 -0700

    Add an example for discard server

---
+++ examples/discard.rs
@@ -0,0 +1,44 @@
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
+extern crate num_cpus;
+extern crate futures;
+extern crate ruyi;
+
+use std::thread;
+
+use futures::{Future, Stream};
+
+use ruyi::reactor::{IntoStream, IntoTask, Task};
+use ruyi::service::tcp::{self, Handler, Session};
+
+#[derive(Clone)]
+struct Discard;
+
+impl Handler for Discard {
+    fn handle(&mut self, session: Session) -> Task {
+        session
+            .into_stream()
+            .for_each(|data| {
+                debug!("Discard:\n{}", data.as_hex_dump());
+                Ok(())
+            })
+            .map_err(|e| error!("{}", e))
+            .into_task()
+    }
+}
+
+fn main() {
+    // Initialize logger
+    env_logger::init().unwrap();
+
+    let n = num_cpus::get();
+    match tcp::Server::with_handler(Discard)
+        .port(10009)
+        .num_of_workers(n)
+        .start() {
+        Ok(()) => thread::park(),
+        Err(e) => error!("{}", e),
+    }
+}
