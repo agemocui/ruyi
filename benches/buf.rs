c0d75287461d701b750edffb63c12425ece70887
Agemo Cui <agemocui@live.com>
2017-07-15 11:11:43 -0700

    Add bench_append_slice and bench_append_vec

--- benches/buf.rs
+++ benches/buf.rs
@@ -10,6 +10,7 @@ use test::Bencher;
 
 const SIZE: usize = 16 * 1024;
 
+
 #[bench]
 fn bench_find(b: &mut Bencher) {
     let mut buf = ByteBuf::with_capacity(SIZE);
@@ -24,4 +25,32 @@ fn bench_windows(b: &mut Bencher) {
     buf.append(u8s::filling(0, SIZE), u8s::append_fill).unwrap();
     let needle = b"\r\n";
     b.iter(|| { buf.windows(needle.len()).position(|w| w == needle); });
+}
+
+const BYTES_SIZE: usize = 1024 * 6;
+
+#[bench]
+fn bench_append_slice(b: &mut Bencher) {
+    let mut bytes = Vec::with_capacity(BYTES_SIZE);
+    unsafe {
+        bytes.set_len(BYTES_SIZE);
+    }
+    b.iter(|| for _ in 0..100 {
+        let bytes2 = bytes.to_vec();
+        let mut buf = ByteBuf::with_capacity(SIZE);
+        buf.append(bytes2.as_slice(), u8s::append).unwrap();
+    });
+}
+
+#[bench]
+fn bench_append_vec(b: &mut Bencher) {
+    let mut bytes = Vec::with_capacity(BYTES_SIZE);
+    unsafe {
+        bytes.set_len(BYTES_SIZE);
+    }
+    b.iter(|| for _ in 0..100 {
+        let bytes2 = bytes.to_vec();
+        let mut buf = ByteBuf::with_capacity(SIZE);
+        buf.append(bytes2, u8s::append_vec).unwrap();
+    });
 }
