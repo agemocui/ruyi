37675c24c423e313b11ed9576e16f1ad29e7d8d0
Agemo Cui <agemocui@live.com>
2017-07-14 22:05:57 -0700

    Add an example for daytime server

---
+++ examples/daytime.rs
@@ -0,0 +1,48 @@
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
+extern crate num_cpus;
+extern crate chrono;
+extern crate futures;
+extern crate ruyi;
+
+use std::thread;
+
+use chrono::prelude::Local;
+use futures::{future, Sink, Future};
+
+use ruyi::buf::ByteBuf;
+use ruyi::reactor::{IntoSink, IntoTask, Task};
+use ruyi::service::tcp::{self, Handler, Session};
+
+#[derive(Clone)]
+struct DayTime;
+
+impl Handler for DayTime {
+    fn handle(&mut self, session: Session) -> Task {
+        future::result(session.set_nodelay(true))
+            .and_then(|_| {
+                let mut time = Local::now().to_rfc2822();
+                time.push_str("\r\n");
+                let response = ByteBuf::from(time.into_bytes());
+                session.into_sink().send(response)
+            })
+            .map_err(|e| error!("{}", e))
+            .into_task()
+    }
+}
+
+fn main() {
+    // Initialize logger
+    env_logger::init().unwrap();
+
+    let n = num_cpus::get();
+    match tcp::Server::with_handler(DayTime)
+        .port(10013)
+        .num_of_workers(n)
+        .start() {
+        Ok(()) => thread::park(),
+        Err(e) => error!("{}", e),
+    }
+}
