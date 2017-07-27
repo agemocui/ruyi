805179d9100c606e608b3614c5e76750010cdc19
Agemo Cui <agemocui@live.com>
2017-07-27 00:25:53 -0700

    More log info

--- examples/pingpong_client.rs
+++ examples/pingpong_client.rs
@@ -1,5 +1,10 @@
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+
 extern crate getopts;
 extern crate num_cpus;
+extern crate chrono;
 extern crate futures;
 extern crate ruyi;
 
@@ -12,15 +17,18 @@ use std::process;
 use std::sync::Arc;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::thread;
+use std::time::Duration;
 
+use chrono::prelude::Utc;
 use futures::{future, stream, Future, Sink, Stream};
+use env_logger::LogBuilder;
 
 use ruyi::buf::ByteBuf;
 use ruyi::channel::spsc;
 use ruyi::nio;
 use ruyi::io::{self as rio, AsyncRead, AsyncWrite};
 use ruyi::net;
-use ruyi::reactor::{self, IntoTask, Task};
+use ruyi::reactor::{self, IntoTask, Task, Timer};
 use ruyi::stream::IntoStream;
 use ruyi::sink::IntoSink;
 
@@ -201,64 +209,80 @@ impl AsyncWrite for TcpStream {
     }
 }
 
+struct Vars {
+    msgs: usize,
+    bytes: usize,
+    conns: usize,
+    count: Arc<AtomicUsize>,
+}
+
 #[inline]
-fn ping_pong(
-    addr: &SocketAddr,
-    len: usize,
-    msgs: &'static mut usize,
-    bytes: &'static mut usize,
-) -> Task {
+fn ping_pong(addr: &SocketAddr, len: usize, vars: &'static mut Vars) -> Task {
     net::TcpStream::connect(addr)
         .and_then(move |s| {
+            vars.conns -= 1;
+            if vars.conns == 0 {
+                if vars.count.as_ref().fetch_sub(1, Ordering::Relaxed) == 1 {
+                    info!("All connected");
+                }
+            }
             future::result(s.set_nodelay(true)).and_then(move |_| {
                 let (r, w) = rio::split(TcpStream(s));
                 let mut data = Vec::<u8>::with_capacity(len);
                 unsafe { data.set_len(len) };
                 w.into_sink().send_all(
                     stream::once(Ok(ByteBuf::from(data))).chain(r.into_stream().filter(move |b| {
-                        *msgs += 1;
-                        *bytes += b.len();
+                        vars.msgs += 1;
+                        vars.bytes += b.len();
                         true
                     })),
                 )
             })
         })
-        .map_err(|e| eprintln!("{}", e))
+        .map_err(|e| error!("{}", e))
         .into_task()
 }
 
 #[inline]
 fn run(conf: Conf) {
-    println!("Pingpong client start: {:?}", conf);
-    let sleep = reactor::sleep(conf.seconds);
+    info!("Start - {:?}", conf);
+    let timer = Timer::new(Duration::from_secs(conf.seconds));
     let mut threads = Vec::with_capacity(conf.threads);
     let total_msgs = Arc::new(AtomicUsize::new(0));
     let total_bytes = Arc::new(AtomicUsize::new(0));
+    let n = Arc::new(AtomicUsize::new(conf.threads));
     for _ in 0..conf.threads {
-        let (tx, rx) = spsc::sync_channel::<Conf>(1).unwrap();
+        let (tx, rx) = spsc::sync_channel(1).unwrap();
         let total_msgs = total_msgs.clone();
         let total_bytes = total_bytes.clone();
+        let conns = conf.connections;
+        let n = n.clone();
         let handle = thread::spawn(move || {
-            let mut msgs = 0usize;
-            let mut bytes = 0usize;
+            let mut vars = Vars {
+                msgs: 0,
+                bytes: 0,
+                conns: conns,
+                count: n,
+            };
             {
-                let task = rx.into_stream().for_each(|conf| {
-                    for _ in 0..conf.connections {
-                        let s_msgs: &'static mut usize = unsafe { mem::transmute(&mut msgs) };
-                        let s_bytes: &'static mut usize = unsafe { mem::transmute(&mut bytes) };
-                        reactor::spawn(ping_pong(&conf.addr, conf.bytes, s_msgs, s_bytes));
+                let task = rx.into_stream().for_each(|(addr, bytes)| {
+                    for _ in 0..conns {
+                        let s_vars: &'static mut Vars = unsafe { mem::transmute(&mut vars) };
+                        reactor::spawn(ping_pong(&addr, bytes, s_vars));
                     }
                     Ok(())
                 });
                 reactor::run(task).unwrap();
             }
-            total_msgs.as_ref().fetch_add(msgs, Ordering::Relaxed);
-            total_bytes.as_ref().fetch_add(bytes, Ordering::Relaxed);
+            total_msgs.as_ref().fetch_add(vars.msgs, Ordering::Relaxed);
+            total_bytes
+                .as_ref()
+                .fetch_add(vars.bytes, Ordering::Relaxed);
         });
-        tx.send(conf).unwrap();
+        tx.send((conf.addr, conf.bytes)).unwrap();
         threads.push((Some(handle), Some(tx)));
     }
-    reactor::run(sleep).unwrap();
+    reactor::run(timer).unwrap();
     for thread in threads.iter_mut() {
         thread.1 = None;
     }
@@ -267,25 +291,38 @@ fn run(conf: Conf) {
     }
     let bytes = total_bytes.as_ref().load(Ordering::Relaxed);
     let msgs = total_msgs.as_ref().load(Ordering::Relaxed);
-    println!("Total bytes read: {}", bytes);
-    println!("Total messages read: {}", msgs);
-    println!("Average message size: {}", bytes as f64 / msgs as f64);
-    println!(
+    info!("Total bytes read: {}", bytes);
+    info!("Total messages read: {}", msgs);
+    info!("Average message size: {}", bytes as f64 / msgs as f64);
+    info!(
         "Throughput: {} MiB/s",
         bytes as f64 / (conf.seconds * 1024 * 1024) as f64
     );
-    println!("Pingpong client end");
 }
 
 #[inline]
 fn exit(err_msg: &str) -> ! {
     if !err_msg.is_empty() {
-        eprintln!("{}", err_msg);
+        error!("{}", err_msg);
     }
     process::exit(1)
 }
 
 fn main() {
+    let mut builder = LogBuilder::new();
+    builder.format(|r| {
+        format!(
+            "{} {:<5} {}",
+            Utc::now().format("%Y-%m-%d %H:%M:%S.%f"),
+            r.level(),
+            r.args()
+        )
+    });
+    if let Ok(v) = env::var("RUST_LOG") {
+        builder.parse(&v);
+    }
+    builder.init().unwrap();
+
     match process_command_line() {
         Ok(Some(conf)) => run(conf),
         Ok(None) => {}
